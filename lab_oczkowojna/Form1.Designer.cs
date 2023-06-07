f8590458e1faa85844bdb0b73068301ebf5016ed
aaugustynka <aaugustynka.alicja@gmail.com>
2023-06-07 16:29:29 +0200

    wojna dodana (do poprawy)

--- lab_oczkowojna/Form1.Designer.cs
+++ lab_oczkowojna/Form1.Designer.cs
@@ -31,13 +31,25 @@
             this.wojna = new System.Windows.Forms.RadioButton();
             this.oczko = new System.Windows.Forms.RadioButton();
             this.label1 = new System.Windows.Forms.Label();
+            this.playButton = new System.Windows.Forms.Button();
+            this.restartButton = new System.Windows.Forms.Button();
+            this.playerCardPictureBox = new System.Windows.Forms.PictureBox();
+            this.computerCardPictureBox = new System.Windows.Forms.PictureBox();
+            this.playerWarCardPictureBox = new System.Windows.Forms.PictureBox();
+            this.computerWarCardPictureBox = new System.Windows.Forms.PictureBox();
+            this.playerCardsLabel = new System.Windows.Forms.Label();
+            this.computerCardsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerCardPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerWarCardPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerWarCardPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // wojna
             // 
             this.wojna.AutoSize = true;
             this.wojna.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.wojna.Location = new System.Drawing.Point(17, 58);
+            this.wojna.Location = new System.Drawing.Point(17, 47);
             this.wojna.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.wojna.Name = "wojna";
             this.wojna.Size = new System.Drawing.Size(91, 24);
@@ -45,12 +57,13 @@
             this.wojna.TabStop = true;
             this.wojna.Text = "WOJNA";
             this.wojna.UseVisualStyleBackColor = true;
+            this.wojna.CheckedChanged += new System.EventHandler(this.wojna_CheckedChanged);
             // 
             // oczko
             // 
             this.oczko.AutoSize = true;
             this.oczko.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.oczko.Location = new System.Drawing.Point(17, 82);
+            this.oczko.Location = new System.Drawing.Point(17, 71);
             this.oczko.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.oczko.Name = "oczko";
             this.oczko.Size = new System.Drawing.Size(88, 24);
@@ -58,15 +71,91 @@
             this.oczko.TabStop = true;
             this.oczko.Text = "OCZKO";
             this.oczko.UseVisualStyleBackColor = true;
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(128, 25);
             this.label1.TabIndex = 2;
             this.label1.Text = "Wybierz grę";
+            // 
+            // playButton
+            // 
+            this.playButton.Location = new System.Drawing.Point(118, 192);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(78, 49);
+            this.playButton.TabIndex = 3;
+            this.playButton.Text = "PLAY";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click_1);
+            // 
+            // restartButton
+            // 
+            this.restartButton.Location = new System.Drawing.Point(76, 297);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(120, 31);
+            this.restartButton.TabIndex = 4;
+            this.restartButton.Text = "RESTART";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click_1);
+            // 
+            // playerCardPictureBox
+            // 
+            this.playerCardPictureBox.Location = new System.Drawing.Point(355, 41);
+            this.playerCardPictureBox.Name = "playerCardPictureBox";
+            this.playerCardPictureBox.Size = new System.Drawing.Size(122, 71);
+            this.playerCardPictureBox.TabIndex = 5;
+            this.playerCardPictureBox.TabStop = false;
+            this.playerCardPictureBox.Click += new System.EventHandler(this.playerCardPictureBox_Click);
+            // 
+            // computerCardPictureBox
+            // 
+            this.computerCardPictureBox.Location = new System.Drawing.Point(337, 158);
+            this.computerCardPictureBox.Name = "computerCardPictureBox";
+            this.computerCardPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.computerCardPictureBox.TabIndex = 6;
+            this.computerCardPictureBox.TabStop = false;
+            // 
+            // playerWarCardPictureBox
+            // 
+            this.playerWarCardPictureBox.Location = new System.Drawing.Point(294, 297);
+            this.playerWarCardPictureBox.Name = "playerWarCardPictureBox";
+            this.playerWarCardPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.playerWarCardPictureBox.TabIndex = 7;
+            this.playerWarCardPictureBox.TabStop = false;
+            this.playerWarCardPictureBox.Click += new System.EventHandler(this.playerWarCardPictureBox_Click);
+            // 
+            // computerWarCardPictureBox
+            // 
+            this.computerWarCardPictureBox.Location = new System.Drawing.Point(377, 329);
+            this.computerWarCardPictureBox.Name = "computerWarCardPictureBox";
+            this.computerWarCardPictureBox.Size = new System.Drawing.Size(100, 50);
+            this.computerWarCardPictureBox.TabIndex = 8;
+            this.computerWarCardPictureBox.TabStop = false;
+            this.computerWarCardPictureBox.Click += new System.EventHandler(this.computerWarPictureBox_Click);
+            // 
+            // playerCardsLabel
+            // 
+            this.playerCardsLabel.AutoSize = true;
+            this.playerCardsLabel.Location = new System.Drawing.Point(556, 115);
+            this.playerCardsLabel.Name = "playerCardsLabel";
+            this.playerCardsLabel.Size = new System.Drawing.Size(85, 16);
+            this.playerCardsLabel.TabIndex = 9;
+            this.playerCardsLabel.Text = "Player Cards";
+            this.playerCardsLabel.Click += new System.EventHandler(this.playerCardsLabel_Click);
+            // 
+            // computerCardsLabel
+            // 
+            this.computerCardsLabel.AutoSize = true;
+            this.computerCardsLabel.Location = new System.Drawing.Point(577, 225);
+            this.computerCardsLabel.Name = "computerCardsLabel";
+            this.computerCardsLabel.Size = new System.Drawing.Size(101, 16);
+            this.computerCardsLabel.TabIndex = 10;
+            this.computerCardsLabel.Text = "ComputerCards";
+            this.computerCardsLabel.Click += new System.EventHandler(this.computerCardsLabel_Click);
             // 
             // Form1
             // 
@@ -74,12 +163,23 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.computerCardsLabel);
+            this.Controls.Add(this.playerCardsLabel);
+            this.Controls.Add(this.computerWarCardPictureBox);
+            this.Controls.Add(this.playerWarCardPictureBox);
+            this.Controls.Add(this.computerCardPictureBox);
+            this.Controls.Add(this.playerCardPictureBox);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.playButton);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.oczko);
             this.Controls.Add(this.wojna);
             this.Name = "Form1";
             this.Text = "WOJNA LUB OCZKO ;)";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.playerCardPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerCardPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.playerWarCardPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerWarCardPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,6 +190,14 @@
         private System.Windows.Forms.RadioButton wojna;
         private System.Windows.Forms.RadioButton oczko;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.PictureBox playerCardPictureBox;
+        private System.Windows.Forms.PictureBox computerCardPictureBox;
+        private System.Windows.Forms.PictureBox playerWarCardPictureBox;
+        private System.Windows.Forms.PictureBox computerWarCardPictureBox;
+        private System.Windows.Forms.Label playerCardsLabel;
+        private System.Windows.Forms.Label computerCardsLabel;
     }
 }
 
--- lab_oczkowojna/Form1.cs
+++ lab_oczkowojna/Form1.cs
@@ -1,26 +1,265 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace lab_oczkowojna
 {
     public partial class Form1 : Form
     {
+        private List<Card> playerDeck;
+        private List<Card> computerDeck;
+        private List<Card> warCards;
+        private Random random;
+        private bool gameStarted;
+
         public Form1()
         {
             InitializeComponent();
+            InitializeGame();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void InitializeGame()
+        {
+            // Inicjalizacja talii kart
+            playerDeck = new List<Card>();
+            computerDeck = new List<Card>();
+            warCards = new List<Card>();
+            random = new Random();
+
+            // Utworzenie talii kart
+            var suits = Enum.GetValues(typeof(CardSuit));
+            var ranks = Enum.GetValues(typeof(CardRank));
+            foreach (CardSuit suit in suits)
+            {
+                foreach (CardRank rank in ranks)
+                {
+                    var card = new Card(suit, rank);
+                    playerDeck.Add(card);
+                }
+            }
+
+            // Tasowanie talii kart
+            for (int i = 0; i < playerDeck.Count; i++)
+            {
+                var temp = playerDeck[i];
+                int randomIndex = random.Next(playerDeck.Count);
+                playerDeck[i] = playerDeck[randomIndex];
+                playerDeck[randomIndex] = temp;
+            }
+
+            // Podział talii kart pomiędzy gracza a komputer
+            for (int i = 0; i < playerDeck.Count; i++)
+            {
+                if (i % 2 == 0)
+                    computerDeck.Add(playerDeck[i]);
+                else
+                    playerDeck[i].IsFaceUp = true;
+            }
+
+            gameStarted = false;
+        }
+
+        private void UpdateUI()
+        {
+            playerCardsLabel.Text = $"Karty gracza: {playerDeck.Count}";
+            computerCardsLabel.Text = $"Karty komputera: {computerDeck.Count}";
+        }
+
+        private void StartGame()
+        {
+            if (!gameStarted)
+            {
+                gameStarted = true;
+                warCards.Clear();
+                UpdateUI();
+                playButton.Enabled = false;
+
+                if (playerDeck.Count == 0 || computerDeck.Count == 0)
+                {
+                    MessageBox.Show("Gra zakończona!", "Koniec gry");
+                    return;
+                }
+
+                var playerCard = playerDeck[0];
+                var computerCard = computerDeck[0];
+
+                playerDeck.RemoveAt(0);
+                computerDeck.RemoveAt(0);
+
+                playerCardPictureBox.Image = playerCard.GetCardImage();
+                computerCardPictureBox.Image = computerCard.GetCardImage();
+
+                if (wojna_CheckedChanged.Checked)
+                {
+                    if (playerCard.Rank > computerCard.Rank)
+                    {
+                        playerDeck.Add(playerCard);
+                        playerDeck.Add(computerCard);
+                    }
+                    else if (playerCard.Rank < computerCard.Rank)
+                    {
+                        computerDeck.Add(computerCard);
+                        computerDeck.Add(playerCard);
+                    }
+                    else
+                    {
+                        warCards.Add(playerCard);
+                        warCards.Add(computerCard);
+                        War();
+                    }
+                }
+                else
+                {
+                    if (playerCard.Rank != computerCard.Rank)
+                    {
+                        if (playerCard.Rank > computerCard.Rank)
+                        {
+                            playerDeck.Add(playerCard);
+                            playerDeck.Add(computerCard);
+                        }
+                        else
+                        {
+                            computerDeck.Add(computerCard);
+                            computerDeck.Add(playerCard);
+                        }
+                    }
+                    else
+                    {
+                        warCards.Add(playerCard);
+                        warCards.Add(computerCard);
+                        War();
+                    }
+                }
+
+                UpdateUI();
+            }
+        }
+
+        private void War()
+        {
+            if (playerDeck.Count < 4 || computerDeck.Count < 4)
+            {
+                MessageBox.Show("Nie można kontynuować wojny, nie ma wystarczającej ilości kart!", "Koniec gry");
+                return;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (playerDeck.Count > 0)
+                {
+                    var playerWarCard = playerDeck[0];
+                    playerDeck.RemoveAt(0);
+                    playerWarCardPictureBox.Image = playerWarCard.GetCardImage();
+                    warCards.Add(playerWarCard);
+                }
+
+                if (computerDeck.Count > 0)
+                {
+                    var computerWarCard = computerDeck[0];
+                    computerDeck.RemoveAt(0);
+                    computerWarCardPictureBox.Image = computerWarCard.GetCardImage();
+                    warCards.Add(computerWarCard);
+                }
+            }
+
+            StartGame();
+        }
+
+        private void wojna_CheckedChanged(object sender, EventArgs e)
+        {
+            if (wojna_CheckedChanged.Checked)
+            {
+                // Wyczyść stos wojny
+                warCards.Clear();
+                playerWarCardPictureBox.Image = null;
+                computerWarCardPictureBox.Image = null;
+            }
+        }
+
+        private void playButton_Click_1(object sender, EventArgs e)
+        {
+            StartGame();
+        }
+
+        private void restartButton_Click_1(object sender, EventArgs e)
+        {
+            InitializeGame();
+            playerCardPictureBox.Image = null;
+            computerCardPictureBox.Image = null;
+            playerWarCardPictureBox.Image = null;
+            computerWarCardPictureBox.Image = null;
+            playButton.Enabled = true;
+        }
+
+        private void playerCardPictureBox_Click(object sender, EventArgs e)
         {
 
         }
 
+        private void playerWarCardPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void computerWarPictureBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playerCardsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void computerCardsLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Card
+    {
+        public CardSuit Suit { get; private set; }
+        public CardRank Rank { get; private set; }
+        public bool IsFaceUp { get; set; }
+
+        public Card(CardSuit suit, CardRank rank)
+        {
+            Suit = suit;
+            Rank = rank;
+            IsFaceUp = false;
+        }
+
+        public Image GetCardImage()
+        {
+            string imageName = IsFaceUp ? $"{Rank}_{Suit}.png" : "card_back.png";
+            return Image.FromFile(imageName);
+        }
+    }
+
+    public enum CardSuit
+    {
+        Spades,
+        Hearts,
+        Diamonds,
+        Clubs
+    }
+
+    public enum CardRank
+    {
+        Two = 2,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
     }
 }
